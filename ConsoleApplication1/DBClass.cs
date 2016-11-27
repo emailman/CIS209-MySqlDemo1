e83bb070f479a03aa5c1a84245ce4f49e13ff988
emailman <emailman@dtcc.edu>
2016-11-27 16:48:22 -0500

    Add project files.

---
+++ ConsoleApplication1/DBClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    result = false;
+                else
+                {
+                    // Strings with connection information
+                    string serverName = "phpmyadmin.cdgwdgkn5fuv.us-west-2.rds.amazonaws.com";
+                    string uidName = "csc164db_ron";
+                    string passwordName = "ra0965";
+
+                    // Build the connection string
+                    string connstring = string.Format
+                        ("SERVER={0}; PORT=3306; DATABASE={1}; UID={2}; PASSWORD={3};",
+                        serverName, databaseName, uidName, passwordName);
+
+                    // Create a connection
+                    connection = new MySqlConnection(connstring);
+
+                    // Try to open the connection
+                    try
+                    {
+                        connection.Open();
+                        result = true;
+                    }
+                    catch
+                    {
+                        result = false;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a db connection object
+            var dbCon = DBConnection.Instance();
+
+            // Set the name of the database
+            dbCon.DatabaseName = "csc164db_ron";
+
+            // Try to connect to the database
+            if (dbCon.IsConnect())
+            {
+                // If successful, try to read records from a table and display them
+                string query = "SELECT * FROM StudentsX";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+
+                try
+                {
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string someStringFromColumnOne = reader.GetString(1);
+                        string someStringFromColumnTwo = reader.GetString(2);
+                        Console.WriteLine(someStringFromColumnOne + "," + someStringFromColumnTwo);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Database query failed :(");
+                }
+                dbCon.Close();
+            }
+            else
+            {
+                Console.WriteLine("Database connection failed :(");
+            }
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
